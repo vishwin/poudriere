c5df9e1758e0d99ef5f0a6661893a91d749ebadc
Charlie Li <git@vishwin.info>
2021-01-20 16:04:55 -0500

    Allow zstd-compressed tarballs in WRKDIR_ARCHIVE_FORMAT

--- src/share/poudriere/common.sh
+++ src/share/poudriere/common.sh
@@ -3528,6 +3528,7 @@ save_wrkdir() {
 	tgz) COMPRESSKEY="z" ;;
 	tbz) COMPRESSKEY="j" ;;
 	txz) COMPRESSKEY="J" ;;
+	tzst) COMPRESSKEY="-zstd" ;;
 	esac
 	unlink ${tarname}
 
@@ -3535,7 +3536,7 @@ save_wrkdir() {
 	    WRKDIR wrkdir || \
 	    err 1 "Failed to lookup WRKDIR for ${originspec}"
 
-	tar -s ",${mnt}${wrkdir%/*},," -c${COMPRESSKEY}f "${tarname}" \
+	tar -s ",${mnt}${wrkdir%/*},," -cf "${tarname}" -${COMPRESSKEY} \
 	    "${mnt}${wrkdir}" > /dev/null 2>&1
 
 	job_msg "Saved ${COLOR_PORT}${originspec} | ${pkgname}${COLOR_RESET} wrkdir to: ${tarname}"
@@ -7919,7 +7920,7 @@ if [ -e "${POUDRIERE_DATA}" ]; then
 fi
 : ${WRKDIR_ARCHIVE_FORMAT="tbz"}
 case "${WRKDIR_ARCHIVE_FORMAT}" in
-	tar|tgz|tbz|txz);;
+	tar|tgz|tbz|txz|tzst);;
 	*) err 1 "invalid format for WRKDIR_ARCHIVE_FORMAT: ${WRKDIR_ARCHIVE_FORMAT}" ;;
 esac
 
